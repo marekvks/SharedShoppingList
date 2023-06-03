c1e74fe9b983366af49e2c022bfedf85725e018e
Marek Vykysalý <mara.vyky@gmail.com>
2023-06-04 01:56:10 +0200

    feat: add forgot password page

---
+++ src/routes/forgor-password/+page.svelte
@@ -0,0 +1,14 @@
+<img src="https://media4.giphy.com/media/cO39srN2EUIRaVqaVq/giphy.gif" alt="forgor">
+
+<style lang="scss">
+    :global(body) {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+    }
+
+    img {
+        height: 400px;
+    }
+</style>
